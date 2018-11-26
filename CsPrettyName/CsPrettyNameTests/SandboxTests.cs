491004f62918d28dce3f3536f050185935e7b923
SS <zakktanaka@hotmail.com>
2018-11-26 22:13:20 +0900

    test

--- CsPrettyName/CsPrettyNameTests/SandboxTests.cs
+++ CsPrettyName/CsPrettyNameTests/SandboxTests.cs
@@ -11,9 +11,48 @@ namespace CsPrettyName.Tests
     [TestClass()]
     public class SandboxTests
     {
+        private static string PrettyName(Type type)
+        {
+            if (type.GetGenericArguments().Length == 0)
+            {
+                return string.Format($"{type.Namespace}.{type.Name}");
+            }
+            var typeDefeninition = string.Format($"{type.Namespace}.{type.Name}");
+            var unmangledName = typeDefeninition.Substring(0, typeDefeninition.IndexOf("`"));
+            var genericArguments = type.GetGenericArguments();
+            return unmangledName + "<" + String.Join(",", genericArguments.Select(PrettyName)) + ">";
+        }
+
+        enum Hoge { A,B,}
+        interface IHoge { }
+
         [TestMethod()]
         public void PrettyNameTest()
         {
+            {
+                var type = typeof(double[]);
+                Console.WriteLine(PrettyName(type));
+            }
+            {
+                var type = typeof(long?);
+                Console.WriteLine(PrettyName(type));
+            }
+            {
+                var type = typeof(Hoge);
+                Console.WriteLine(PrettyName(type));
+            }
+            {
+                var type = typeof(IHoge);
+                Console.WriteLine(PrettyName(type));
+            }
+            {
+                var type = typeof(IEnumerable<SandboxTests>);
+                Console.WriteLine(PrettyName(type));
+            }
+            {
+                var type = typeof(Tuple<string, int?, IEnumerable<SandboxTests>>);
+                Console.WriteLine(PrettyName(type));
+            }
             Assert.Fail();
         }
     }
