afcbaa7d696d5486e6ad5510274c0c42e91c1e90
SS <zakktanaka@hotmail.com>
2018-11-26 21:56:43 +0900

    initial commit

---
+++ CsPrettyName/CsPrettyNameTests/SandboxTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CsPrettyName;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsPrettyName.Tests
+{
+    [TestClass()]
+    public class SandboxTests
+    {
+        [TestMethod()]
+        public void PrettyNameTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
